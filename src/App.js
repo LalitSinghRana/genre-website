88bab377404ab2e94f2fb39aa1464915fce1b12a
Lalit Rana <lalit.rana@nagarro.com>
2021-08-01 23:52:34 +0530

    plain react started

--- src/App.js
+++ src/App.js
@@ -1,82 +1,13 @@
-import React from 'react';
-import 'antd/dist/antd.css';
+import React, { useState } from 'react';
 import './App.css';
-
-import { Layout, Menu, Carousel, Divider } from 'antd';
-import { RightOutlined, LeftOutlined } from '@ant-design/icons';
-const { Header } = Layout;
 
 function App() {
 	return (
-		<>
-			<Layout>
-				<Header className='header'>
-					<div className='logo' />
-					<Menu theme='dark' mode='horizontal' defaultSelectedKeys={['1']}>
-						<Menu.Item key='1'>nav 1</Menu.Item>
-						<Menu.Item key='2'>nav 2</Menu.Item>
-						<Menu.Item key='3'>nav 3</Menu.Item>
-					</Menu>
-				</Header>
-			</Layout>
-			<Carousel
-				arrows
-				nextArrow={<RightOutlined />}
-				prevArrow={<LeftOutlined />}
-			>
-				<div className='carousel-div'>
-					<h2>heading 1</h2>
-					<Divider>Tags on some websites</Divider>
-					<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
-						alias similique, quas nostrum soluta sed quaerat aut excepturi,
-						assumenda explicabo mollitia distinctio, voluptatibus praesentium.
-						Non, nam at! Odit, assumenda quis?
-					</p>
-					<Divider>The shortcoming of these approaches</Divider>
-					<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
-						alias similique, quas nostrum soluta sed quaerat aut excepturi,
-						assumenda explicabo mollitia distinctio, voluptatibus praesentium.
-						Non, nam at! Odit, assumenda quis?
-					</p>
-					<Divider>In new method</Divider>
-					<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
-						alias similique, quas nostrum soluta sed quaerat aut excepturi,
-						assumenda explicabo mollitia distinctio, voluptatibus praesentium.
-						Non, nam at! Odit, assumenda quis?
-					</p>
-				</div>
-				<div className='carousel-div'>
-					<h2>heading 2</h2>
-					<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
-						alias similique, quas nostrum soluta sed quaerat aut excepturi,
-						assumenda explicabo mollitia distinctio, voluptatibus praesentium.
-						Non, nam at! Odit, assumenda quis?
-					</p>
-				</div>
-				<div className='carousel-div'>
-					<h2>heading 3</h2>
-					<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
-						alias similique, quas nostrum soluta sed quaerat aut excepturi,
-						assumenda explicabo mollitia distinctio, voluptatibus praesentium.
-						Non, nam at! Odit, assumenda quis?
-					</p>
-				</div>
-				<div className='carousel-div'>
-					<h2>heading 4</h2>
-					<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
-						alias similique, quas nostrum soluta sed quaerat aut excepturi,
-						assumenda explicabo mollitia distinctio, voluptatibus praesentium.
-						Non, nam at! Odit, assumenda quis?
-					</p>
-				</div>
-			</Carousel>
-		</>
+		<div id='outer-div'>
+			<div id='top-div'>
+				<h4>Find Sub-Genre of any movie/book/TV series</h4>
+			</div>
+		</div>
 	);
 }
 
