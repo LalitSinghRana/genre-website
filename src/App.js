f73d9e706d87ca89da0d667a2ea2cbc0ca5adf4b
Lalit Rana <lalit.rana@nagarro.com>
2021-08-03 00:49:00 +0530

    scrollable text added using newsticker

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import React, { useState } from 'react';
 import './App.css';
+import NewsTicker from 'react-advanced-news-ticker';
 
 function App() {
+	var arr = ['movie', 'book', 'comic', 'TV series', 'games'];
 	return (
 		<div id='outer-div'>
 			<div id='top-div'>
-				<h4>Find Sub-Genre of any movie/book/TV series</h4>
+				<div id='top-div-1'>
+					<h4>Find Sub-Genre of any</h4>
+					<NewsTicker
+						rowHeight={60}
+						maxRows={1}
+						speed={600}
+						duration={10000}
+						autoStart={true}
+						pauseOnHover={true}
+						id='nt-example2'
+					>
+						{arr.map((item, i) => (
+							<h4 className='hour'>{item}</h4>
+						))}
+					</NewsTicker>
+				</div>
 			</div>
-			<div id="bottom-div">
-				
-			</div>
+			<div id='bottom-div'></div>
 		</div>
 	);
 }
