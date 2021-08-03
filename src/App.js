c59ea199cf740cf35593a9bcc8fc37690c360ee8
Lalit Rana <lalit.rana@nagarro.com>
2021-08-04 00:52:09 +0530

    top-div CSS finished

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 import NewsTicker from 'react-advanced-news-ticker';
 
 function App() {
-	var arr = ['movie', 'book', 'comic', 'TV series', 'games'];
+	var arr = ['Movie', 'Novel', 'Comic Book', 'TV Series', 'Video Game'];
 	return (
 		<div id='outer-div'>
 			<div id='top-div'>
-				<div id='top-div-1'>
-					<h4>Find Sub-Genre of any</h4>
-					<NewsTicker
-						rowHeight={60}
-						maxRows={1}
-						speed={600}
-						duration={10000}
-						autoStart={true}
-						pauseOnHover={true}
-						id='nt-example2'
-					>
-						{arr.map((item, i) => (
-							<h4 className='hour'>{item}</h4>
-						))}
-					</NewsTicker>
-				</div>
+				<h4>Find genre, sub-genre and tags of any</h4>
+				<NewsTicker
+					rowHeight={100}
+					maxRows={1}
+					speed={500}
+					duration={3000}
+					autoStart={true}
+					pauseOnHover={true}
+					id='nt-example2'
+				>
+					{arr.map((item, i) => (
+						<h4 key={i}>{item}</h4>
+					))}
+				</NewsTicker>
 			</div>
 			<div id='bottom-div'></div>
 		</div>
